67ac93b449e95d75f78511b28bd001970a2d18f2
Brandon White <bjwhite@fnal.gov>
2019-10-22 17:20:48 -0500

    Station starts. Not sure if it works yet, but it starts.

---
+++ sam_station/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# ensure the sam station is properly set up to run
+pip install --no-deps --no-cache-dir -e /opt/sam/sam_station/sam-station
+
+# Make the logging directory
+mkdir /var/log/sam
+
+#bin/sam_station /opt/sam/sam_station/sam_station_config.yaml
+tail -f /dev/null
