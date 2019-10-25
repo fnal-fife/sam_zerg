b264d65b4b303c61ee828666d9000d425895badc
Brandon White <bjwhite@fnal.gov>
2019-10-25 13:18:07 -0500

    Dockerized SAM Station works now. Can run projects for the sam_bjwhite experiment.

--- sam_station/docker-entrypoint.sh
+++ sam_station/docker-entrypoint.sh
@@ -6,5 +6,5 @@ pip install --no-deps --no-cache-dir -e /opt/sam/sam_station/sam-station
 # Make the logging directory
 mkdir /var/log/sam
 
-#bin/sam_station /opt/sam/sam_station/sam_station_config.yaml
-tail -f /dev/null
+bin/sam_station /opt/sam/sam_station/sam_station_config.yaml
+#tail -f /dev/null
--- samweb_server/docker-entrypoint.sh
+++ samweb_server/docker-entrypoint.sh
@@ -5,6 +5,8 @@ pip install --no-deps --no-cache-dir -e /opt/sam/samweb_server/sam-web
 
 # Get the database password from the secret
 export DBPASS=$(cat /run/secrets/samweb_db_secret)
+# Get the web server ip address that we need to give back to clients outside the Docker network
+export SAM_ZERG_APACHE_IP=$(nslookup sam_zerg_apache_1 | grep -A2 "Non-authoritative" | tail -1 | awk '{print $NF}')
 envsubst < /opt/sam/samweb_server/samweb_server.conf > /opt/sam/samweb_server/samweb_server.conf
 
 if [ $# -gt 0 ]; then
