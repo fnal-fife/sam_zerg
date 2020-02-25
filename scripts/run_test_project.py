b6df45a4fc038a7fbf2316fc4415dafd4ae2bbab
Brandon White <bjwhite@fnal.gov>
2020-02-25 13:21:52 -0600

    Add more stuff to project test script.

--- scripts/run_test_project.py
+++ scripts/run_test_project.py
@@ -4,23 +4,30 @@ import samweb_client
 
 def main():
     #samweb = samweb_client.SAMWebClient(experiment='samdev')
-    samweb = samweb_client.SAMWebClient(experiment='sam_bjwhite')
     #station = 'samdev'
-    station = 'bjwhite_python_station'
-    group = 'samdev'
+    #group = 'samdev'
 
-    definition = 'bjwhite_ifdh_test'
-    #definition = 'bjwhite_enstore_dataset_1'
+    #samweb = samweb_client.SAMWebClient(experiment='sam_bjwhite')
+    #station = 'bjwhite_python_station'
+    #group = 'samdev'
+
+    samweb = samweb_client.SAMWebClient(experiment='gm2')
+    station = 'gm2'
+    group = 'gm2'
+
+    #definition = 'bjwhite_ifdh_test' # SAMDEV
+    #definition = 'bjwhite_enstore_dataset_1' # SAMDEV
+
+    definition = 'poms_depends_466544_1' # GM2
 
     proj_name = time.strftime('bjwhite_ifdh_test_%Y%m%d%H_%%d') % os.getpid()
 
     print 'Using station: ', station
-    #print samweb.locateFile('ifdh_test_file_0')
     print samweb.descDefinition(definition)
     
     cpurl = samweb.startProject(project=proj_name, defname=definition, station=station, user='bjwhite', group=group)
-    print 'Sleeping 10sec'
-    time.sleep(10)
+    print 'Sleeping 3 sec'
+    time.sleep(3)
     cpurl = samweb.findProject(proj_name, station)
     consumer_id = samweb.startProcess(cpurl, 'demo', 'demo', 'demo', node='fermicloud172.fnal.gov', user='bjwhite')
     print 'got cpurl, consumer_id: ', cpurl, consumer_id
@@ -35,12 +42,9 @@ def main():
     try:
         while furi:
             #if flag:
-            #    samweb.setProcessFileStatus(consumer_url, f_info['filename'], 'transferred')
-            #    time.sleep(1)
-            #    samweb.setProcessFileStatus(consumer_url, f_info['filename'], 'consumed')
             #    flag = False
+            #    samweb.setProcessFileStatus(consumer_url, f_info['filename'], 'consumed')
             #else:
-            #    samweb.setProcessFileStatus(consumer_url, f_info['filename'], 'skipped')
             #    flag = True
             samweb.setProcessFileStatus(consumer_url, f_info['filename'], 'transferred')
             time.sleep(1)
