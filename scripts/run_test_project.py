8c9ce354d3d9a3ec38d38ce53809e64bbbc2611f
Brandon White <bjwhite@fnal.gov>
2020-03-18 16:49:06 -0500

    Just adding this to keep what I have been doing

--- scripts/run_test_project.py
+++ scripts/run_test_project.py
@@ -7,18 +7,17 @@ def main():
     #station = 'samdev'
     #group = 'samdev'
 
-    #samweb = samweb_client.SAMWebClient(experiment='sam_bjwhite')
-    #station = 'bjwhite_python_station'
-    #group = 'samdev'
+    samweb = samweb_client.SAMWebClient(experiment='sam_bjwhite')
+    station = 'bjwhite_python_station'
+    group = 'samdev'
 
-    samweb = samweb_client.SAMWebClient(experiment='gm2')
-    station = 'gm2'
-    group = 'gm2'
+    #samweb = samweb_client.SAMWebClient(experiment='gm2')
+    #station = 'gm2'
+    #group = 'gm2'
 
-    #definition = 'bjwhite_ifdh_test' # SAMDEV
+    definition = 'bjwhite_ifdh_test' # SAMDEV
     #definition = 'bjwhite_enstore_dataset_1' # SAMDEV
-
-    definition = 'poms_depends_466544_1' # GM2
+    #definition = 'poms_depends_466544_1' # GM2
 
     proj_name = time.strftime('bjwhite_ifdh_test_%Y%m%d%H_%%d') % os.getpid()
 
