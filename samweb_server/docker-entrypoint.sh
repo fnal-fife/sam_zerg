ebe4de2d33c8a4e14997ff20cafeccd6bad081d9
Brandon White <bjwhite@fnal.gov>
2023-11-13 15:16:18 -0700

    Start up the trifecta, and run a SAM project against sam_bjwhite @ localhost:8483. Uses SAMDEV database.

--- samweb_server/docker-entrypoint.sh
+++ samweb_server/docker-entrypoint.sh
@@ -6,10 +6,9 @@ pip install --no-deps --no-cache-dir -e /opt/sam/samweb_server/sam-web
 # Get the database password from the secret
 export DBPASS=$(cat /run/secrets/samweb_db_secret)
 echo "Set the database password from the secret to: $DBPASS"
-# Get the web server ip address that we need to give back to clients outside the Docker network
-export SAM_ZERG_APACHE_IP=$(nslookup apache | grep -A2 "Non-authoritative" | tail -1 | awk '{print $NF}')
-echo "Set the server ip to: $SAM_ZERG_APACHE_IP"
-envsubst < /opt/sam/samweb_server/samweb_server.conf > /opt/sam/samweb_server/samweb_server.conf
+
+# Replace placeholders in configuration file with runtime values.
+envsubst < /opt/sam/samweb_server/samweb_server.conf.template > /opt/sam/samweb_server/samweb_server.conf
 echo "SAM Web Server configuration:\n$(cat /opt/sam/samweb_server/samweb_server.conf)"
 
 if [ $# -gt 0 ]; then
