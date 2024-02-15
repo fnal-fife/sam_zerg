9f02f39fc29e2faf07980e797a84aae6c4c1ed16
Brandon White <bjwhite@fnal.gov>
2024-02-15 16:05:17 -0700

    Parameterize generation of the SAM web server configuration better.

--- samweb_server/docker-entrypoint.sh
+++ samweb_server/docker-entrypoint.sh
@@ -3,9 +3,11 @@
 # ensure samweb is properly set up to run
 pip install --no-deps --no-cache-dir -e /opt/sam/samweb_server/sam-web
 
-# Get the database password from the secret
-export DBPASS=$(cat /run/secrets/samweb_db_secret)
-echo "Set the database password from the secret to: $DBPASS"
+# Get the database password from the secret (only for podman, otherwise use envar from secret mount)
+if [ -f /run/secrets/samweb_db_secret ]; then
+    export DB_PASS=$(cat /run/secrets/samweb_db_secret)
+    echo "Set the database password from the secret to: $DBPASS"
+fi
 
 # Replace placeholders in configuration file with runtime values.
 envsubst < /opt/sam/samweb_server/samweb_server.conf.template > /opt/sam/samweb_server/samweb_server.conf
