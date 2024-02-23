d0ae17269c015375b3bd102cdbf5a1998821f9d6
Brandon White <bjwhite@fnal.gov>
2024-02-23 08:11:07 -0700

    Replace station monitor w/ its actual repository.

--- sam_station_monitor/time_functions.py
+++
@@ -1,8 +0,0 @@
-
-from datetime import datetime
-from psycopg2.tz import LocalTimezone, FixedOffsetTimezone
-
-localtimezone=LocalTimezone()
-utctimezone=FixedOffsetTimezone()
-
-epoch = datetime(1970,1,1,tzinfo=utctimezone)
