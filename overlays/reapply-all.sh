e99a3dc44f8c5f413ed3b85c2c7aa1e20b6b3230
Brandon White <bjwhite@fnal.gov>
2024-05-14 13:19:48 -0600

    Add the rest to reapply-all.sh

--- overlays/reapply-all.sh
+++ overlays/reapply-all.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-oc project samdev; cd samdev; make apply; cd ..;
 oc project samannie; cd annie/; make apply; cd ..;
+oc project samdune; cd dune/; make apply; cd ..;
 oc project samgm2; cd gm2/; make apply; cd ..;
+oc project samicarus; cd icarus/; make apply; cd ..;
+oc project samlariat; cd lariat/; make apply; cd ..;
 oc project samminerva; cd minerva/; make apply; cd ..;
 oc project samminos; cd minos/; make apply; cd ..;
+oc project sammu2e; cd mu2e/; make apply; cd ..;
+oc project samnova; cd nova/; make apply; cd ..;
+oc project samdev; cd samdev; make apply; cd ..;
 oc project samsbn; cd sbn/; make apply; cd ..;
 oc project samsbnd; cd sbnd/; make apply; cd ..;
-oc project samdune; cd dune/; make apply; cd ..;
-oc project samlariat; cd lariat/; make apply; cd ..;
-oc project samnova; cd nova/; make apply; cd ..;
 oc project samuboone; cd uboone/; make apply; cd ..;
-oc project samicarus; cd icarus/; make apply; cd ..;
